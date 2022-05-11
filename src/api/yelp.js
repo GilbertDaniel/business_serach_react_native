048503b2b13afce21f6e695173d393db8de1be06
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-11 23:15:07 +0630

    Configur axios

---
+++ src/api/yelp.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorizaton: 'Bearer DZpZNt-82s_mrvwCCirrcZdF00J-gc3PjP6JpUDUWXupNp48tvc3tXHm5Z_ZuO5Yo_Kdl7q5GiI0n4Fu9FbEdC1Dzu6DPns-4qbqFIJhaKjTlREr76WgaXaHtYx7YnYx'
+    }
+});
