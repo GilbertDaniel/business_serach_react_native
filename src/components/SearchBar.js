bfb18cae046720b16023d75049665961f7a6c6ef
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-11 22:58:36 +0630

    Managing State for Search Bar Input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,17 @@ import {View, TextInput, StyleSheet} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange}) => {
     return (
         <View style={styles.backgroundStyle}>
             <Feather name="search" style={styles.iconStyle}/>
             <TextInput 
+                autoCapitalize='none'
+                autoCorrect={false}
                 style={styles.inputStyle}
                 placeholder='Search'
+                value={term}
+                onChangeText={newTerm => onTermChange(newTerm)}
             />
         </View>
     );
