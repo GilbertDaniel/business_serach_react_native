51b6e1ba4c8aebe5bc47ed5c63d28137382c7bfb
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-14 23:10:37 +0630

    Extracting Hook Logic for Search Results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,38 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const searchApi = async (searchTerm) => {
-   try{
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term: searchTerm,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
-   }catch (e){
-    // console.log(e);
-    setErrorMessage("Something Want Wrong!");
-   }
-  };
-
-  // Call Search Api when component is first render
-  //searchApi('pasta')
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
-      <SearchBar 
-        term={term} 
-        onTermChange={setTerm} 
-        onTermSubmit={() => searchApi(term)} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
