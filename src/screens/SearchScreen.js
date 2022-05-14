5f72e9e1a9cabb3442bfcf081b90e8d1f235db0f
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-14 22:46:50 +0630

    Using Outside API Yelp and Making Request, Error Handling, Initial Search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,24 +1,44 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-    const [term, setTerm] = useState('');
-    return (
-        <View>
-            <SearchBar 
-                term={term} onTermChange={newTerm => 
-                setTerm(newTerm)}
-                onTermSubmit={() => console.log('term was submitted')}
-            />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
-        </View>
-    );
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) => {
+   try{
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: searchTerm,
+        location: 'san jose'
+      }
+    });
+    setResults(response.data.businesses);
+   }catch (e){
+    // console.log(e);
+    setErrorMessage("Something Want Wrong!");
+   }
+  };
+
+  // Call Search Api when component is first render
+  //searchApi('pasta')
+
+  return (
+    <View>
+      <SearchBar 
+        term={term} 
+        onTermChange={setTerm} 
+        onTermSubmit={() => searchApi(term)} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
