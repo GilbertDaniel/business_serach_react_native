95568a5b89064bd8c4a591d3a738fcbcd6aa58b2
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-17 18:46:47 +0630

    Add if condition for no result to show in resultlist

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -3,6 +3,9 @@ import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native
 import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 const ResultsList = ({ title, results, navigation }) => {
+  if(!results.length){
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
